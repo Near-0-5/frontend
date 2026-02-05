f02dca2a0da58325839f44940886e2eb4439cd31
anneedesign-ui <anneedesign@gmail.com>
2026-02-05 11:32:27 +0900

    chore: fix component export order(#77)

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,13 +1,13 @@
 export { default as ConcertCard } from './common/card/ConcertCard';
 export { default as OngoingLiveCard } from './common/card/OngoingLiveCard';
 
+export { default as CategorySection } from './common/CategorySection';
 export { default as Dropdown } from './common/dropdown/Dropdown';
+
 export { default as DropdownContent } from './common/dropdown/DropdownContent';
 export { default as DropdownItem } from './common/dropdown/DropdownItem';
 export { default as DropdownSeparator } from './common/dropdown/DropdownSeparator';
 export { default as DropdownTrigger } from './common/dropdown/DropdownTrigger';
-
-export { default as CategorySection } from './common/CategorySection';
 export { default as FollowButton } from './common/FollowButton';
 
 export { default as Modal } from './common/modal/Modal';
